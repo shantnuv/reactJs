30fa745747f2e64d57da89402715a50aec6383d0
Shantnu Vashistha <shantnu@Apples-MacBook-Pro.local>
2018-02-21 19:29:18 +0530

    Login form complete without redux

---
+++ bookworm/bookworm-react/src/components/forms/LoginForm.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Form, Button} from 'semantic-ui-react';
+import Validator from 'validator';
+import PropTypes from 'prop-types';
+import InlineError from '../messages/InlineError';
+
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: {
+        email: '',
+        password: '',
+      },
+      loading: false,
+      errors: {},
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({
+      data: {...this.state.data, [event.target.type]: event.target.value}
+    });
+  };
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({errors});
+
+    if (!Object.keys(errors).length) {
+      this.props.submit(this.state.data);
+    }
+  };
+
+  validate = (data) => {
+    const errors = {};
+      if(!Validator.isEmail(data.email)) {
+        errors.email = 'Incorrect email.';
+      }
+      if(!Object.keys(data.password).length) {
+        errors.password = 'Password can\'t be blank';
+      }
+
+    return errors;
+  };
+
+  render() {
+    const {data, errors} = this.state;
+    return (
+      <div>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.email}>
+          <label htmlFor='email'>Email</label>
+          <input type='email'
+                 name='email'
+                 id='email'
+                 placeholder='example@example.com'
+                 value={data.email}
+                 onChange={this.onChange}/>
+          {errors.email && <InlineError text={errors.email}/>}
+        </Form.Field>
+        <Form.Field error={!!errors.password}>
+          <label htmlFor='password'>Email</label>
+          <input type='password'
+                 name='password'
+                 id='password'
+                 placeholder='Make it secure'
+                 value={data.password}
+                 onChange={this.onChange}/>
+          {errors.password && <InlineError text={errors.password}/>}
+
+        </Form.Field>
+
+        <Button primary>Login</Button>
+      </Form>
+
+      </div>
+    );
+  };
+};
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
+
+export default LoginForm;
