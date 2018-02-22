908c47f9563e5117294ff96affcc262cea8eaa20
Shantnu Vashistha <shantnu@Apples-MacBook-Pro.local>
2018-02-22 23:36:20 +0530

    redux initial setup done

--- bookworm/bookworm-react/src/index.js
+++ bookworm/bookworm-react/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import store from './store';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './rootReducer';
-
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
   <BrowserRouter>
